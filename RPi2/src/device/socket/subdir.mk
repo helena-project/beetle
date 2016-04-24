b3142303b2138338988b140cbf9ecf011c443564
James Hong <james.hong@cs.stanford.edu>
2016-04-23 21:59:12 -0700

    Attempt to build on rasp pi.

--- RPi2/src/device/socket/subdir.mk
+++ RPi2/src/device/socket/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/device/socket/%.o: ../src/device/socket/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++1y -D__cplusplus=201402L -I../lib/include -I../include -O1 -Wall -c -fmessage-length=0 -m32 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -D__cplusplus=201402L -I../lib/include -I../include -O1 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- RPi2/src/device/socket/tcp/subdir.mk
+++ RPi2/src/device/socket/tcp/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 src/device/socket/tcp/%.o: ../src/device/socket/tcp/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++1y -D__cplusplus=201402L -I../lib/include -I../include -O1 -Wall -c -fmessage-length=0 -m32 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -D__cplusplus=201402L -I../lib/include -I../include -O1 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
