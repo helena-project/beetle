f122d4e7771168642cbea5e223dd59784fe567b6
James Hong <james.hong@cs.stanford.edu>
2016-04-30 02:41:06 -0700

    update script

--- controller/runserver.sh
+++ controller/runserver.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 ./manage.py runserver 0.0.0.0:80 &
-./manage.py runsslserver 0.0.0.0:443
+
+if ["$#" -eq 3] 
+	then
+	./manage.py runsslserver 0.0.0.0:443 --certificate "$1" --key "$2"
+else
+	./manage.py runsslserver 0.0.0.0:443
+fi
 
 pkill -f './manage.py runserver'
 pkill -f './manage.py runsslserver'
