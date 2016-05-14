3a4eed57898fae74b2e17c65ca62e749eb4417d0
James Hong <james.hong@cs.stanford.edu>
2016-05-14 03:30:25 -0700

    More instrumentation for client.

--- client/BeetleClient.py
+++ client/BeetleClient.py
@@ -7,6 +7,7 @@
 import argparse
 import os
 import re
+import sys
 import signal
 import socket
 import ssl
@@ -14,6 +15,12 @@ import struct
 import threading
 import time
 import traceback
+import numpy as np
+
+import lib.att as att
+
+def getTimeMillis():
+	return int(time.time() * 1000)
 
 def getArguments():
 	"""
@@ -26,6 +33,8 @@ def getArguments():
 		help="port the server is runnng on")
 	parser.add_argument("--measure", "-m", action='store_true', 
 		help="print performance measurements")
+	parser.add_argument("--debug", "-d", action='store_true', 
+		help="print debugging")
 	return parser.parse_args()
 
 args = getArguments()
@@ -35,6 +44,9 @@ s = ssl.wrap_socket(s, cert_reqs=ssl.CERT_NONE)	# TODO fix this
 s.connect((args.host, args.port))
 
 lastRequestTime = 0.0
+
+# Allow only one transaction at a time
+transactSema = threading.Semaphore(1)
 
 def outputPrinter(s):
 	"""
@@ -52,15 +64,20 @@ def outputPrinter(s):
 				chunk = s.recv(n - received)
 				if chunk == "":
 					raise RuntimeError("socket connection broken")
+				if received == 0:
+					opcode = ord(chunk[0])
 				received += len(chunk)
 				chunks.append(" ".join(x.encode('hex') for x in chunk))
 			print " ".join(chunks)
 
-			if args.measure: # TODO check if response
-				now = int(time.time() * 1000)
-				print "request at: %d" % (lastRequestTime,)
-				print "response at: %d" % (now,)
-				print "elapsed: %d" % (now - lastRequestTime,)
+			if att.isResponse(opcode) or opcode == att.OP_ERROR:
+				if args.measure:
+					now = getTimeMillis()
+					print "request at: %d" % (lastRequestTime,)
+					print "response at: %d" % (now,)
+					print "elapsed: %d" % (now - lastRequestTime,)
+				transactSema.release()
+
 	except Exception, err:
 		print "Exception in output thread:", err
 		os.kill(os.getpid(), signal.SIGTERM)
@@ -104,6 +121,19 @@ outputThread.start()
 # Regexes to match convenience commands.
 writePattern = re.compile(r"^write (?P<handle>[\d+]+) (?P<value>.*)$")
 readPattern = re.compile(r"^read (?P<handle>[\d+]+)$")
+repeatPattern = re.compile(r"^repeat (?P<ntimes>\d*) (?P<value>.*)$")
+
+def sendMessage(s, message):
+	opcode = message[0]
+	if att.isRequest(opcode):
+		transactSema.acquire()
+		global lastRequestTime
+		lastRequestTime = getTimeMillis()
+
+	if s.send(chr(len(message))) != 1:
+		raise RuntimeError("failed to write length prefix")
+	if s.send(message) != len(message):
+		raise RuntimeError("failed to write packet")
 
 def inputReader(s):
 	"""
@@ -119,15 +149,17 @@ def inputReader(s):
 		else:
 			previousLine = line
 		
+		nTimes = 1
 		try: 
 			writeRequest = writePattern.match(line)
 			readRequest = readPattern.match(line)
+			repeatRequest = repeatPattern.match(line)
 			if writeRequest is not None:
 				command = writeRequest.groupdict()
 				message = bytearray()
 
 				handle = int(eval(command["handle"]))
-				message.append(0x12)
+				message.append(att.OP_WRITE_REQ)
 				message.append(handle & 0xFF)
 				message.append(handle >> 8)
 				value = command["value"]
@@ -136,10 +168,16 @@ def inputReader(s):
 			elif readRequest is not None:
 				command = readRequest.groupdict()
 				message = bytearray()
-				message.append(0x0A)
+				message.append(att.OP_READ_REQ)
 				handle = int(eval(command["handle"]))
 				message.append(handle & 0xFF)
 				message.append(handle >> 8)
+			elif repeatRequest is not None:
+				command = repeatRequest.groupdict()
+				nTimes = int(command["ntimes"])
+				value = command["value"]
+				value = value.replace(" ", "")
+				message = bytearray.fromhex(value)
 			else:
 				line = line.replace(" ","")
 				message = bytearray.fromhex(line)
@@ -147,16 +185,38 @@ def inputReader(s):
 			print "Invalid input:", err
 			continue
 
-		if True: # TODO check if request
-			global lastRequestTime
-			lastRequestTime = int(time.time() * 1000)
-
 		if len(message) == 0:
 			continue
-		if s.send(chr(len(message))) != 1:
-			raise RuntimeError("failed to write length prefix")
-		if s.send(message) != len(message):
-			raise RuntimeError("failed to write packet")
+
+		if nTimes > 1:
+			isRequest = att.isRequest(message[0])
+			latencies = []
+
+			startTime = getTimeMillis()
+			for _ in xrange(nTimes):
+				sendMessage(s, message)
+				if isRequest == True:
+					transactSema.acquire()
+					latencies.append(getTimeMillis() - lastRequestTime)
+					transactSema.release()
+
+			if isRequest == True: # need to wait for last request to finish
+				transactSema.acquire()
+				endTime = getTimeMillis()
+				transactSema.release()
+			else:
+				endTime = getTimeMillis()
+
+			print "=== repeat ==="
+			print "start: %d" % (startTime,)
+			print "end: %d" % (endTime,)
+			print "elapsed: %dms mean: %0.3fms median: %dms stdev: %0.3fms" % (
+				endTime - startTime, np.mean(latencies), np.median(latencies), 
+				np.std(latencies))
+
+		else:
+			sendMessage(s, message)
+
 		time.sleep(1) # Give beetle some time to respond
 
 try: 
