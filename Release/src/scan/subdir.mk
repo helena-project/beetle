29710fce51c0eddb48f0de1edd876adcf9ff6d32
James Hong <james.hong@cs.stanford.edu>
2016-05-23 03:59:06 -0700

    fix gitignore issue

---
+++ Release/src/scan/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/scan/AutoConnect.cpp \
+../src/scan/Scanner.cpp 
+
+OBJS += \
+./src/scan/AutoConnect.o \
+./src/scan/Scanner.o 
+
+CPP_DEPS += \
+./src/scan/AutoConnect.d \
+./src/scan/Scanner.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/scan/%.o: ../src/scan/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++1y -D__cplusplus=201402L -I../lib/include -I../include -O1 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
