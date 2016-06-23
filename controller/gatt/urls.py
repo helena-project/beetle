205879515f4f1da447c062c34ec808b58891cb35
James Hong <james.hong@cs.stanford.edu>
2016-06-22 21:31:44 -0700

    Misc clean-up

--- controller/gatt/urls.py
+++ controller/gatt/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
 	url(r'^service?/$', views.list_services),
 	url(r'^char?/$', views.list_characteristics),
 	url(r'^desc?/$', views.list_descriptors),
-	url(r'^service/' + uuid("uuid") + r'$', 
+	url(r'^service/' + uuid("uuid") + r'$',
 		views.find_service,
 	 	name='find service by name'),
-	url(r'^char/' + uuid("uuid") + r'$', 
-		views.find_characteristic, 
+	url(r'^char/' + uuid("uuid") + r'$',
+		views.find_characteristic,
 		name='find characteristic by uuid'),
-	url(r'^desc/' + uuid("uuid") + r'$', 
+	url(r'^desc/' + uuid("uuid") + r'$',
 		views.find_descriptor)
 ]
